0845c157ccb3a94ab2c04aecacdececa53429653
is-9108 <syouta103010@gmail.com>
2020-09-17 23:13:54 +0900

    ver2-2en commit

--- calculator/TwoViewController.swift
+++ calculator/TwoViewController.swift
@@ -11,29 +11,24 @@ import GoogleMobileAds
 
 class TwoViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var processLabel: UILabel!
+    
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBOutlet weak var bannerView1: GADBannerView!
     
     @IBOutlet weak var bannerView2: GADBannerView!
     
-    
-    var sum1 = 0
-    
-    var sum2 = 0
-    
-    var sum3 = 0
-    
-    var suma = 0
-    var sumb = 0
-    var sumc = 0
-    
-    var cal = ""
-    
-    var start = false
+    //計算
+    var firstInteger = 0
+    var resultInteger = 0
+    var process = ""
+    var mode = true
+    var ope = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        processLabel.text = process
 //        bannerView1.adUnitID = "ca-app-pub-6004781963877348/8397689293"
 //        bannerView1.rootViewController = self
 //        bannerView1.load(GADRequest())
@@ -45,116 +40,163 @@ class TwoViewController: UIViewController {
     }
     
     @IBAction func zero(_ sender: Any) {
-        if start == false{
-            label.text = ""
+        if mode {
+            if resultLabel.text == "0" {
+                resultLabel.text = "0"
+            }else {
+                resultLabel.text! += "0"
+            }
+        }else{
+            resultLabel.text! = "0"
+            mode = true
         }
-        label.text! += "0"
-        start = true
     }
     
     @IBAction func one(_ sender: Any) {
-        if start == false{
-            label.text = ""
+        if mode {
+            if resultLabel.text == "0" {
+                resultLabel.text = "1"
+            }else {
+                resultLabel.text! += "1"
+            }
+        }else{
+            resultLabel.text! = "1"
+            mode = true
         }
-        label.text! += "1"
-        start = true
     }
     
-//    @IBAction func point(_ sender: Any) {
-//        label.text! += "."
-//        start = true
-//    }
+    func twoToTen(number:String) -> String{
+        var decimal = Int(number)!
+        var base = 1
+        var result = 0
+        while(decimal > 0){
+            result = result + (decimal % 10) * base
+            decimal /= 10
+            base *= 2
+        }
+        return String(result)
+    }
     
-    func twoToTen1(int:Int){
+    func tenToTwo(number:String) -> String{
+        var binary = Int(number)!
         var base = 1
-        var int = int
-        while(int > 0){
-            suma = suma + (int % 10) * base
-            int = int / 10
-            base = base * 2
+        var result = 0
+        while(binary > 0){
+            result = result + (binary % 2) * base
+            binary /= 2
+            base *= 10
         }
-    }
-    func twoToTen2(int:Int){
-        var base = 1
-        var int = int
-        while(int > 0){
-            sumb = sumb + (int % 10) * base
-            int = int / 10
-            base = base * 2
-        }
-    }
-    func tenToTwo(int:Int){
-        var base = 1
-        var int = int
-        while(int > 0){
-            sumc = sumc + (int % 2) * base
-            int = int / 2
-            base = base * 10
-        }
+        return String(result)
+        
     }
     
     @IBAction func equal(_ sender: Any) {
-        twoToTen1(int: sum1)
-        twoToTen2(int: Int(label.text!)!)
-        if cal == "plus"{
-            sum2 = suma + sumb
-            tenToTwo(int: sum2)
-        }else if cal == "minus"{
-            sum2 = suma - sumb
-            tenToTwo(int: sum2)
-        }else if cal == "mul"{
-            sum2 = suma * sumb
-            tenToTwo(int: sum2)
-        }else if cal == "div"{
-            sum2 = suma / sumb
-            tenToTwo(int: sum2)
-        }else{
-            label.text = "\(sum1)"
-        }
-        label.text = "\(sumc)"
+        mode = false
+        calculation()
+        firstInteger = 0
     }
     
     @IBAction func plus(_ sender: Any) {
-        cal = "plus"
-        sum1 += Int(label.text!)!
-        label.text = "0"
-        start = false
+        if resultLabel.text != "0"{
+            calculation()
+        }
+        ope = "+"
+
+        mode = false
     }
     
     @IBAction func minus(_ sender: Any) {
-        cal = "minus"
-        sum1 += Int(label.text!)!
-        label.text = "0"
-        start = false
+        if resultLabel.text != "0"{
+            calculation()
+        }
+        ope = "-"
+
+        mode = false
     }
     
     @IBAction func mul(_ sender: Any) {
-        cal = "mul"
-        sum1 += Int(label.text!)!
-        label.text = "0"
-        start = false
+        if resultLabel.text != "0"{
+            calculation()
+        }
+        ope = "×"
+
+        mode = false
     }
     
     @IBAction func div(_ sender: Any) {
-        cal = "div"
-        sum1 += Int(label.text!)!
-        label.text = "0"
-        start = false
+        if resultLabel.text != "0"{
+            calculation()
+        }
+        ope = "÷"
+
+        mode = false
     }
     
     @IBAction func cancel(_ sender: Any) {
-        sum1 = 0
-        sum2 = 0
-        suma = 0
-        sumb = 0
-        sumc = 0
-        label.text = "0"
-        start = false
+        resultLabel.text = "0"
+        firstInteger = 0
+        process = ""
+        processLabel.text = ""
+        ope = ""
+        mode = true
+    }
+    
+    func calculation(){
+        var result = ""
+        var resultNumber = ""
+        
+        result = twoToTen(number: resultLabel.text!)
+        switch ope {
+        case "":
+            process = resultLabel.text!
+            processLabel.text = process
+            firstInteger += Int(result)!
+        case "+":
+            if resultLabel.text == "0"{
+                process += resultLabel.text!
+            }else{
+                process += "+\(resultLabel.text!)"
+            }
+            processLabel.text = process
+            firstInteger += Int(result)!
+        case "-":
+            if resultLabel.text == "0"{
+                process += resultLabel.text!
+            }else{
+                process += "-\(resultLabel.text!)"
+            }
+            processLabel.text = process
+            mode = false
+            firstInteger -= Int(result)!
+        case "×":
+            if resultLabel.text == "0"{
+                process += resultLabel.text!
+            }else{
+                process += "×\(resultLabel.text!)"
+            }
+            processLabel.text = process
+            mode = false
+            firstInteger *= Int(result)!
+        case "÷":
+            if resultLabel.text == "0"{
+                process += resultLabel.text!
+            }else{
+                process += "÷\(resultLabel.text!)"
+            }
+            processLabel.text = process
+            mode = false
+            firstInteger /= Int(result)!
+        default:
+            print("演算子でのエラー")
+        }
+        resultNumber = tenToTwo(number: String(firstInteger))
+        resultLabel.text = resultNumber
+        
     }
 
     @IBAction func change(_ sender: Any) {
         let change = storyboard?.instantiateViewController(withIdentifier: "Change10") as! Change10ViewController
-        change.sumString = label.text!
+        change.sumString = resultLabel.text!
         change.version = "2"
         present(change,animated: true,completion: nil)
     }
